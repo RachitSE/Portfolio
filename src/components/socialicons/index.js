f240ccf31690bb272a9e5c7ff5d46635cd242af5
RachitCoderAI <sb180189@birlahighschool.com>
2022-12-05 21:57:15 +0530

    My portfolio Commit

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaTwitter,
+  FaYoutube,
+  FaTwitch,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.twitter && (
+          <li>
+            <a href={socialprofils.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+        )}
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Follow Me</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,146 @@
+const logotext = "MY PORTFOLIO";
+const meta = {
+    title: "Rachit's Portfolio",
+    description: "I’m Rachit, a Tech Enthusiast _ Full stack devloper, Learning Ethical Hacking",
+};
+
+const introdata = {
+    title: "I’m Rachit Srivastava",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "My favourite programming language is Java",
+    },
+    description: "Hello, I am Rachit, a 15-year old programmer from India. Currently, I am learning more about deep learning and ethical hacking.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Introduction",
+    aboutme: "Hello, I am Rachit, a 15-year old programmer from India. Currently, I am learning more about deep learning and ethical hacking. I have worked with many clients from all over the world. I want to become a Software Developer and develop new and cool things.",
+};
+const worktimeline = [{
+        jobtitle: "Developer of the year (Westline)",
+        where: "Server : Toronto ",
+        date: "2022",
+    },
+    {
+        jobtitle: "Designer of the month (444K)",
+        where: "Server : Canada",
+        date: "2022",
+    },
+    {
+        jobtitle: "Developer of the month (Ken Apparels)",
+        where: "Server : Canada",
+        date: "2021 - 2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 100,
+    },
+    {
+        name: "Djano",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 100,
+    },
+    {
+        name: "React",
+        value: 89,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I have created many UI/UX designs for many websites all over the world such as Ken Apparels, Westline Roleplays, 444K etc.",
+    },
+    {
+        title: "Plugins",
+        description: "I have an experience of creating many plugins based on Java, C# and C++.",
+    },
+    {
+        title: "Artificial Intelligence",
+        description: "Using Python and Java and using Stack, Pyttsx3, Tensorflow etc. I have created many AI's which can make your task easier. Examples - Voice Recognition, Object Identifier etc.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDT44m1P-Zcw1TN7N5iFAFWF0GLGAyGVT2Rw&usqp=CAU",
+        desctiption: "My JS Projects.",
+        link: "https://github.com/RachitSR-delta",
+    },
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/74/74897.png",
+        desctiption: "My C++ Projects.",
+        link: "https://github.com/RachitSR-delta",
+    },
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/121/121152.png",
+        desctiption: "My Java Projects.",
+        link: "https://github.com/RachitSR-delta",
+    },
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/121/121537.png",
+        desctiption: "My Html, Css Projects.",
+        link: "https://github.com/RachitSR-delta",
+    },
+    {
+        img: "https://www.pngitem.com/pimgs/m/72-723723_paintbrush-black-and-white-png-black-transparent-paint.png",
+        desctiption: "UI/UX Designs",
+        link: "https://github.com/RachitSR-delta",
+    },
+    {
+        img: "https://icones.pro/wp-content/uploads/2021/03/icone-de-configuration-noire.png",
+        desctiption: "My DCLM Configs.",
+        link: "https://github.com/RachitSR-delta",
+    },
+
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/5968/5968286.png",
+        desctiption: "My Python plugins.",
+        link: "https://github.com/RachitSR-delta",
+    },
+    {
+        img: "https://i.pinimg.com/originals/0d/8b/43/0d8b437a4c1c788f036590bc4b71ff55.png",
+        desctiption: "My Discord Bots.",
+        link: "https://github.com/RachitSR-delta",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "fingdev@protonmail.com",
+    YOUR_FONE: "Compiler Charisma#3842",
+    description: "(You can get in contact with me via Discord or Email).",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_qmswvqv",
+    YOUR_TEMPLATE_ID: "template_vxtxb5g",
+    YOUR_USER_ID: "wB7Fyn4ziCdqDUp7O",
+};
+
+const socialprofils = {
+    github: "https://github.com/RachitSR-delta",
+    twitter: "https://twitter.com/InDevNoob",
+    youtube: "https://www.youtube.com/channel/UCcdzYZ4lPJUBySlXCyfdCSw",
+    twitch: "https://www.twitch.tv/compilercharisma",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
